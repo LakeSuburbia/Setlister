1bc3d5537fcaf8561d5b810ac8ce7c76de59f6d2
SanderMangelschots <sander.mangelschots@optimile.eu>
2022-05-13 18:14:00 +0200

    Use spotipy for requests to spotify

--- base/client.py
+++ base/client.py
@@ -1,12 +1,12 @@
-import json
 import time
 from datetime import datetime
-from urllib.parse import urlencode
 
 import requests
+import spotipy
 from requests.structures import CaseInsensitiveDict
+from spotipy.oauth2 import SpotifyOAuth
 
-from .formatting import compare_strings, format_string
+from .formatting import compare_strings
 from .settings import ApiKeys, ApiURLS
 
 apikeys = ApiKeys()
@@ -44,35 +44,36 @@ class SetlistAPI(BaseAPI):
 class SpotifyAPI(BaseAPI):
     def __init__(self):
         self.HEADERS["Authorization"] = apikeys.SPOTIFY
+        scope = "playlist-modify-public"
+        self.user = "21dx3vvgs2hunjbzvezp4dp5q"
+        self.api = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=apikeys.SPOTIFY_CLIENT_ID,
+                client_secret=apikeys.SPOTIFY_CLIENT_SECRET,
+                redirect_uri="http://localhost",
+                scope=scope,
+            )
+        )
 
     def create_playlist(self, artist: str, songs: list[str]):
-        data = json.dumps(
-            {
-                "name": f"{artist} - setlist {datetime.now().year}",
-                "description": f"This is an estimation of what a setlist of {artist} would look like.",
-                "public": True,
-            }
-        )
-        playlist = self.post(
-            f"{apiurls.SPOTIFY}/users/21dx3vvgs2hunjbzvezp4dp5q/playlists",
-            headers=self.HEADERS,
-            data=data,
+        playlist = self.api.user_playlist_create(
+            user=self.user,
+            name=f"{artist} - setlist {datetime.now().year}",
+            public=True,
+            description=f"This is an estimation of what a setlist of {artist} would look like.",
         )["id"]
         song_uris = self._get_songs(artist, songs)
-        new_songs = list(map(lambda x: "spotify%3Atrack%3A" + x, song_uris))
-        add_to_playlist_url = (
-            f"{apiurls.SPOTIFY}/playlists/{playlist}/tracks?uris={','.join(new_songs)}"
+        return self.api.user_playlist_add_tracks(
+            user=self.user, playlist_id=playlist, tracks=song_uris
         )
-        return requests.post(add_to_playlist_url, headers=self.HEADERS)
 
     def _get_songs(self, artist: str, songs: list[str]):
         uris = []
         for song in songs:
             print(f"trying to find {song}")
-            params = urlencode({"q": f"track:{song}, artist:{artist}", "type": "track"})
             try:
-                tracks = self.get(
-                    f"{apiurls.SPOTIFY}/search?{params}", headers=self.HEADERS
+                tracks = self.api.search(
+                    q=f"track:{song}, artist:{artist}", type="track"
                 )["tracks"]["items"]
                 new_track = None
                 for track in tracks:
