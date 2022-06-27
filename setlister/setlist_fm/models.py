1bb855527a1cae3e2c03962cf244453b9fb543b1
SanderMangelschots <sander.mangelschots@optimile.eu>
2022-06-27 19:22:54 +0200

    Create setlistcache

--- setlister/setlist_fm/models.py
+++ setlister/setlist_fm/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class SetlistCache(models.Model):
+    time_created = models.DateTimeField(default=timezone.now())
+    artist = models.CharField(max_length=100)
